d86984f3b4309a783d4d056b6a316cccc338b5ee
hassight <sisalem.hassan@gmail.com>
2018-04-07 22:53:17 +0200

    Changed to 0.5sec

--- src/game/mode/Game.java
+++ src/game/mode/Game.java
@@ -2148,7 +2148,7 @@ public class Game {
 	
 	public void sleepFrame() {
 		try {
- 			Thread.sleep(1000);
+ 			Thread.sleep(500);
  		} catch (InterruptedException e) {
  			e.printStackTrace();
  		}
